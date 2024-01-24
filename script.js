8df6194f2fd94df862d2a5e004f558b1e5072c7c
HossainIdrish <139071336+HossainIdrish@users.noreply.github.com>
2024-01-24 15:05:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -17,11 +17,3 @@ $.getJSON('https://raw.githubusercontent.com/orhuna/WebGIS_SLU_M1/main/Module%20
   // Create and add the heat map layer
   var heat = L.heatLayer(coordinatesOnly).addTo(map);
 });
-
-$.getJSON('https://raw.githubusercontent.com/orhuna/WebGIS_SLU_M1/main/Module%201/Assignment%201/data/rodents.geojson', function(data) {
-  var coordinatesOnly = data.features.map(function(feature) {
-    return [feature.geometry.coordinates[1], feature.geometry.coordinates[0], 1];
-  });
-
-  var heat = L.heatLayer(coordinatesOnly).addTo(map);
-});
